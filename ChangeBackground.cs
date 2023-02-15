4d264c4e85a4d8585eb9d69762e384a1eab9ae8c
Lana-Zbasnik <115007072+Lana-Zbasnik@users.noreply.github.com>
2023-02-14 22:32:19 -0700

    Add files via upload

---
+++ ChangeBackground.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeBackground : MonoBehaviour
+{
+    //public GameObject camping;
+   // public GameObject freev;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BackGroundChanger()
+    {
+       //camping.setActive(false);
+       // freev.setActive(true);
+       SceneManager.LoadScene("welcome_scene");
+    }
+
+}
