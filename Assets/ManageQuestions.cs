43fe247030302cc4c57268746baacf774db1bb82
Hannah <hannah.sethereng@gmail.com>
2023-03-20 10:13:37 -0600

    Multiple choice works

--- Assets/ManageQuestions.cs
+++ Assets/ManageQuestions.cs
@@ -5,6 +5,8 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using static UnityEngine.Random;
+using System.Linq;
+using System;
 
 public class ManageQuestions : MonoBehaviour
 {
@@ -12,48 +14,138 @@ public class ManageQuestions : MonoBehaviour
     [SerializeField] private TextMeshProUGUI Question1;
     [SerializeField] private TextMeshProUGUI Answer2;
     [SerializeField] private TextMeshProUGUI Choice1;
+    [SerializeField] private TextMeshProUGUI Choice2;
+    [SerializeField] private TextMeshProUGUI Counter;
     private int ranNum1;
     private int ranNum2;
     private char addSub;
     private int answer;
-    private int counter;
+    private static int counter = 0;
     private bool correct = false;
+    private static int guess;
+    private static int indexCorrect = -1;
 
 
    public void ButtonPress() { 
+    if(guess == indexCorrect) {
+        counter++;
+        correct = true;
+    }
+    Debug.Log("indexCorrect = " + indexCorrect + ", " + "guess = " + guess);
+    guess = -1;
+    
+    Counter.text = "You have " + counter + " correct answers!";
 
         Question();
-
+       
+        
     }
-
-
-
-
-   private void isCorrect(int guess) {
-       if(guess == answer) {
+ /* private bool isCorrect(String guess1)
+    {
+        correct = false;
+        int guessValue;
+        if (int.TryParse(guess1, out guessValue))
+        {
+            if (guessValue == answer)
             correct = true;
-       }
+            Debug.Log("isCorrect returns: " + correct);
+            return correct;
+        }
+        return false;
+    }*/
+
+
+
+   public void GetGuess() {
+        guess = 0;
    }
 
+   public void GetGuess1() {
+    guess = 1;
+   
+   }
+
+     public void GetGuess2() {
+        guess = 2; 
+   }
+  
+
+    
+
     private void Question() {
+    
      ranNum1 = Range(1,11);
      ranNum2 = Range(1,11);
-     answer = ranNum1 + ranNum2;
 
-     Question1.text = ranNum1 + " + " + ranNum2 + " = " + answer;
-     Answer2.text = "" + answer;
-     Choice1.text = "" + Range(answer-3,answer + 3);
+    float operatorRand = UnityEngine.Random.Range(0f, 1f);
+    if (operatorRand < 0.5f) {
+        addSub = '+';
+        answer = ranNum1 + ranNum2;
+    } 
+    else {
+        addSub = '-';
+        answer = ranNum1 - ranNum2;
+        if (answer < 0) {
+            int temp = ranNum1;
+            ranNum1 = ranNum2;
+            ranNum2 = temp;
+            answer = ranNum1 - ranNum2;
+
+        }
+    }
+
+    int choice1;
+    int choice2;
+
+    Question1.text = ranNum1 + " " + addSub + " " + ranNum2 + " = " + answer;
+
+     choice1 =  Range(answer-5,answer + 5);
+        while (choice1 == answer || choice1 < 0) {
+            choice1 =  Range(answer-5,answer + 5);
+        }
+     
+     choice2 = Range(answer-5,answer + 5);
+            while (choice2 == answer || choice2 == choice1 || choice2 < 0) {
+                choice2 = Range(answer-5,answer + 5);
+            }
+            
+            int[] choices = new int[3];
+        choices[0] = answer;
+        choices[1] = choice1;
+        choices[2] = choice2;
+
+        int n = choices.Length;
+        while (n > 1)
+        {
+            int k = UnityEngine.Random.Range(0, n--);
+            int temp = choices[n];
+            choices[n] = choices[k];
+            choices[k] = temp;
+        }
+
+        Answer2.text = "" + choices[0];
+        Choice1.text = "" + choices[1];
+        Choice2.text = "" + choices[2];
+
+        //figure out which button have the right answer
+        for (int i = 0; i < 3; i++) {
+            if (choices[i] == answer){
+                indexCorrect = i;
+            }
+        }
+        Debug.Log("Correct answer is in: " + indexCorrect);
 
 
+        
      Question1.textStyle = TMP_Style.NormalStyle;
      Answer2.textStyle = TMP_Style.NormalStyle;
-      
+      correct = false;
 
     }
+    
      public void OnClick()
     {
-        SceneManager.LoadScene("Test Scene");
-    }
+        SceneManager.LoadScene("Test Scene");   }
 
 
 }
