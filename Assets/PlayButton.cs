a2171401cffacbf6251a5b09f3c24b33535533b0
Lana-Zbasnik <lanazbasnik13@gmail.com>
2023-04-24 10:19:19 -0600

    the last one

--- Assets/PlayButton.cs
+++ Assets/PlayButton.cs
@@ -6,20 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class PlayButton : MonoBehaviour
 {
-    //public GameObject camping;
-   // public GameObject freev;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
     public void BackGroundChanger()
     {
--- Assets/quit.cs
+++ Assets/quit.cs
@@ -1,14 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
-public class QuitButton : MonoBehaviour
+public class quit : MonoBehaviour
 {
-    //public GameObject camping;
-   // public GameObject freev;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,9 +16,8 @@ public class QuitButton : MonoBehaviour
         
     }
 
-    public void QuitGame()
-    {
-       Application.Quit();
-    }
+    public void Quitgame() {
 
+        Application.Quit();
+    }
 }
